07ddf2a485867f3d0f04733d3716608dc8dc2d7e
Sky3d <sky3ddd@gmail.com>
2017-10-11 13:17:57 +0300

    add client library, main sample

---
+++ cloud4rpi.py
@@ -0,0 +1,90 @@
+# --------------------------------------------------------------------------- #
+#  Cloud4RPi client library for ESP8266, https://github.com/cloud4rpi
+# --------------------------------------------------------------------------- #
+
+import json
+
+# https://raw.githubusercontent.com/micropython/micropython-lib/mqtt/umqtt.simple/umqtt/simple.py
+from mqtt import MQTTClient
+
+
+C4R_BROKER_HOST = 'mq.cloud4rpi.io'
+C4R_BROKER_PORT = 1883
+
+C4R_TOPIC_FORMAT = b'devices/%s/%s'
+
+
+class Device(object):
+    def __init__(self, device_token):
+        self.__device_token = bytes(device_token, 'utf-8')
+        self.__variables = None
+        self.__diag_variables = None
+        self.__mqtt = None
+        self.__bindings = dict()
+
+    def __publish(self, topic, payload=None):
+        if payload is None:
+            return False
+        msg = json.dumps({'payload': payload})
+        self.__mqtt.publish(C4R_TOPIC_FORMAT %
+                            (self.__device_token, topic), msg, qos=1)
+        print(topic, "<--", msg)
+
+    def __on_message(self, topic, msg):
+        print(topic.decode().rsplit('/')[-1], "-->", msg.decode())
+        data = json.loads(msg)
+        for var_name, callback in self.__bindings.items():
+            if var_name in data.keys():
+                ret = callback(data[var_name])
+                self.__publish('data', {var_name: ret})
+
+    def connect(self):
+        self.__mqtt = MQTTClient(client_id=self.__device_token,
+                                 server=C4R_BROKER_HOST,
+                                 port=C4R_BROKER_PORT)
+
+        self.__mqtt.set_callback(self.__on_message)
+        try:
+            self.__mqtt.connect()
+            self.__mqtt.subscribe(C4R_TOPIC_FORMAT %
+                                  (self.__device_token, 'commands'), qos=1)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    def declare(self, variables):
+        self.__variables = variables
+
+    def declare_diag(self, diag_variables):
+        self.__diag_variables = diag_variables
+
+    def publish_config(self):
+        cfg = []
+        for var_name, var_config in self.__variables.items():
+            cfg.append({'name': var_name, 'type': var_config['type']})
+            self.__bindings[var_name] = var_config['bind']
+        self.__publish('config', cfg)
+
+    def publish_diag(self):
+        self.__publish('diagnostics', self.__diag_variables)
+
+    def publish_data(self):
+        for var_name, callback in self.__bindings.items():
+            self.__variables[var_name]['value'] = \
+                                callback(self.__variables[var_name]['value'])
+
+        cfg = {var_name: var_config['value']
+               for var_name, var_config in self.__variables.items()}
+        self.__publish('data', cfg)
+
+    def check_commands(self):
+        self.__mqtt.check_msg()
+
+
+def connect(device_token):
+    device = Device(device_token)
+    if device.connect():
+        return device
+    else:
+        return
---
+++ main.py
@@ -0,0 +1,83 @@
+from machine import Pin, idle, reset
+from network import WLAN, STA_IF
+
+import cloud4rpi
+
+# Put the name of your Wi-Fi and its password here.
+# If you have an open Wi-Fi, simply remove the second item.
+WIFI_SSID_PASSWORD = '__SSID__', '__PSW__'
+
+# Put your device token here. To get the token,
+# sign up at https://cloud4rpi.io and create a device.
+DEVICE_TOKEN = '__YOUR_DEVICE_TOKEN__'
+
+LED_PIN = 12
+BUTTON_PIN = 16
+
+STA = WLAN(STA_IF)
+STA.active(True)
+STA.connect(*WIFI_SSID_PASSWORD)
+
+while not STA.isconnected():
+    idle()
+print("Connected to Wi-Fi")
+
+led = Pin(LED_PIN, Pin.OUT)
+button = Pin(BUTTON_PIN, Pin.IN)
+button_state_prev = button.value()
+button_state_now = button_state_prev
+
+# --------------------------------------------------------------------------- #
+
+btn_value = False
+
+def on_led(value):
+    led.value(not value)
+    return not led.value()
+
+def get_btn(value):
+    global btn_value
+    return btn_value
+
+device = cloud4rpi.connect(DEVICE_TOKEN)
+
+if not device:
+    print("Failed to connect to Cloud4RPi")
+else:
+    print("Connected to Cloud4RPi")
+    device.declare({
+        'LED': {
+            'type': 'bool',
+            'value': False,
+            'bind': on_led
+        },
+        'Button': {
+            'type': 'bool',
+            'value': False,
+            'bind': get_btn
+        }
+    })
+    device.declare_diag({
+        'IP Address': STA.ifconfig()[0]
+    })
+
+    device.publish_diag()
+    device.publish_config()
+    device.publish_data()
+
+    print("Waiting for messages...")
+    while True:
+        try:
+            device.check_commands()
+        except Exception as e:
+            print("%s: %s; Reconnecting..." % (type(e).__name__, e))
+            if device.connect():
+                print("Success!")
+            else:
+                print("Failed to reconnect")
+
+        button_state_prev = button_state_now
+        button_state_now = button.value()
+        if button_state_prev == 1 and button_state_now == 0:
+            btn_value = not btn_value
+            device.publish_data()
